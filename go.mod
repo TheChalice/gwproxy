7e68bbe92f5921fa6b6c83d3f0e32164c7543f6b
jiangtong <jiangtong@asisinfo.com>
2019-07-26 14:27:00 +0800

    type

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module gwproxy
 
 go 1.12
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/ghodss/yaml v1.0.0
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+	github.com/google/gofuzz v1.0.0
+	github.com/spf13/pflag v1.0.3 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.2.2
+)
--- main.go
+++ main.go
@@ -1,33 +1,42 @@
 package main
 
 import (
+	"bytes"
 	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"gwproxy/externols/github.com/openshift/origin/deploy/api/v1"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"regexp"
+	"strconv"
 )
 
 type handle struct {
 	host string
 }
+type kinds struct {
+	Kind string `json:"kind"`
+}
 
 func regurl(r *http.Request) string {
 
-	url :=r.URL.String()
+	url := r.URL.String()
 
-	clusterurl :=""
+	clusterurl := ""
 
 	regws := regexp.MustCompile("access_token")
 
 	if regws.MatchString(url) {
 
-		clusterurl=r.FormValue("Cluster")
+		clusterurl = r.FormValue("Cluster")
 
-	}else {
+	} else {
 
-		clusterurl=r.Header["Cluster"][0]
+		clusterurl = r.Header["Cluster"][0]
 
 	}
 	return clusterurl
@@ -35,25 +44,65 @@ func regurl(r *http.Request) string {
 
 func (this *handle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-	cluster:=regurl(r)
-
+	cluster := regurl(r)
 	//fmt.Printf("cluster\n%+v\n",cluster)
-
 	//fmt.Printf("Header\n %+v\n", r.Header["Origin"][0])
-
-	remote, err := url.Parse("https://"+cluster)
+	remote, err := url.Parse("https://" + cluster)
 
 	if err != nil {
 		panic(err)
 	}
 	proxy := httputil.NewSingleHostReverseProxy(remote)
 
+	proxy.ModifyResponse = rewriteBody
+
 	proxy.Transport = &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
+
 	proxy.ServeHTTP(w, r)
 }
+func rewriteBody(resp *http.Response) (err error) {
+	b, err := ioutil.ReadAll(resp.Body) //Read html
 
+	if err != nil {
+		return err
+	}
+	err = resp.Body.Close()
+	if err != nil {
+		return err
+	}
+
+	kinds := kinds{}
+
+	err = json.Unmarshal(b, &kinds)
+
+	if err != nil {
+		return err
+	}
+
+	if kinds.Kind == "DeploymentConfig" {
+		DeploymentConfig := deploy.DeploymentConfig{}
+
+		err = json.Unmarshal(b, &DeploymentConfig)
+		//if err!=nil{
+		//	return err
+		//}
+		fmt.Printf("Namespace \n%+v\n", DeploymentConfig.ObjectMeta.Namespace)
+	}
+
+	b = bytes.Replace(b, []byte("server"), []byte("schmerver"), -1) // replace html
+
+	body := ioutil.NopCloser(bytes.NewReader(b))
+
+	resp.Body = body
+
+	resp.ContentLength = int64(len(b))
+
+	resp.Header.Set("Content-Length", strconv.Itoa(len(b)))
+
+	return nil
+}
 func startServer() {
 	//被代理的服务器host和port
 	h := &handle{}
